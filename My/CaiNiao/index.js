9d79a428afb0843ee06c42b1aecce7a1be85a81b
chao.wang <chao.wang@elanking.com>
2018-07-26 11:21:37 +0800

    node的异步操作

--- My/CaiNiao/index.js
+++ My/CaiNiao/index.js
@@ -1,5 +1,19 @@
-let http = require('http')
-http.createServer((req, res) => {
-    res.setHeader('Content-type', 'text/plain;charset=utf8')
-    res.end('中文')
-}).listen(2000)
+// let http = require('http')
+// let fs = require('fs')
+
+// http.createServer((req, res) => {
+//     res.setHeader('Content-type', 'text/plain;charset=utf8')
+//     res.end('中文')
+// }).listen(2000)
+
+
+
+// var fs = require("fs");
+// var data = fs.readFileSync('1.txt');
+// console.log(data.toString());
+// console.log("程序执行结束!");
+
+
+
+
+
