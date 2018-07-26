788d96b933752e897a2dfc83cc59c08d2a87d11f
chao.wang <chao.wang@elanking.com>
2018-07-26 13:53:21 +0800

    时间

--- My/CaiNiao/index.js
+++ My/CaiNiao/index.js
@@ -15,5 +15,41 @@
 
 
 
+// 异步
+// var fs = require("fs");
+// fs.readFile('1.txt', function (err, data) {
+//     if (err) return console.error(err);
+//     console.log(data.toString());
+// });
+// console.log("程序执行结束!");
+
+
+
+// 引入 events 模块
+var events = require('events');
+// 创建 eventEmitter 对象
+var eventEmitter = new events.EventEmitter();
+// 绑定事件及事件的处理程序
+eventEmitter.on('eventName', eventHandler);
+// 触发事件
+eventEmitter.emit('eventName');
+function eventHandler() {
+    console.log("eventName时间触发，eventHandler函数执行")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
