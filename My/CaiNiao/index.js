cab5d3654aa0cda115a5be60a64dc5268cfa7227
chao.wang <chao.wang@elanking.com>
2018-07-26 17:30:07 +0800

    可写流

--- My/CaiNiao/index.js
+++ My/CaiNiao/index.js
@@ -244,6 +244,37 @@
 
 
 
+// 写入流
+// var fs = require("fs");
+// var data = '菜鸟教程官网地址：www.runoob.com';
+// // 创建一个可以写入的流，写入到文件 output.txt 中
+// var writerStream = fs.createWriteStream('2.txt');
+// // 使用 utf8 编码写入数据
+// writerStream.write(data,'UTF8');
+// // 标记文件末尾
+// writerStream.end();
+// // 处理流事件 --> data, end, and error
+// writerStream.on('finish', function() {
+//     console.log("写入完成。");
+// });
+// writerStream.on('error', function(err){
+//    console.log(err.stack);
+// });
+// console.log("程序执行完毕");
+
+
+
+// // 自己重写
+// let fs = require('fs')
+// let data = '这里是文字'
+// let writerStream = fs.createWriteStream('2.txt')
+// writerStream.write(data, 'utf8')
+// writerStream.end()
+// writerStream.on('error', (err) => {
+//     console.log(err)
+// })
+
+
 
 
 
