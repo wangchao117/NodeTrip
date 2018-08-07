b11bcc72ef11c84fc442b4a79fcad6f0853c2329
chao.wang <chao.wang@elanking.com>
2018-08-07 14:11:12 +0800

    对象结构赋值的默认值

--- ES6/1.js
+++ ES6/1.js
@@ -38,14 +38,63 @@
 // let { foo: baz } = { foo: "aaa", bar: "bbb" };
 // baz // "aaa"
 // foo // error: foo is not defined
+// // 上面代码中，foo是匹配的模式，baz才是变量。真正被赋值的是变量baz，而不是模式foo。
 
 
 
 
 
+// // 与数组一样，解构也可以用于嵌套结构的对象。
+// let obj = {
+//     p: [
+//         'Hello',
+//         { y: 'World' }
+//     ]
+// };
+
+// let { 
+//     p: [
+//         x, 
+//         { y }
+//     ] 
+// } = obj;
+// x // "Hello"
+// y // "World"
+// // 注意，这时p是模式，不是变量，因此不会被赋值。如果p也要作为变量赋值，可以写成下面这样。
+// let obj = {
+//     p: [
+//       'Hello',
+//       { y: 'World' }
+//     ]
+//   };
+  
+//   let { p, p: [x, { y }] } = obj;
+//   x // "Hello"
+//   y // "World"
+//   p // ["Hello", {y: "World"}]
 
 
 
+
+
+// // 对象的解构也可以指定默认值。
+// var {x = 3} = {};
+// x // 3
+// var {x, y = 5} = {x: 1};
+// x // 1
+// y // 5
+// var {x: y = 3} = {};
+// y // 3
+// var {x: y = 3} = {x: 5};
+// y // 5
+// var { message: msg = 'Something went wrong' } = {};
+// msg // "Something went wrong"
+// // 默认值生效的条件是，对象的属性值严格等于undefined。
+// var {x = 3} = {x: undefined};
+// x // 3
+// var {x = 3} = {x: null};
+// x // null
+// // 上面代码中，属性x等于null，因为null与undefined不严格相等，所以是个有效的赋值，导致默认值3不会生效。
 
 
 
