52c7d7078d5880bed912c493c082e0cb8d18e285
chao.wang <chao.wang@elanking.com>
2018-08-09 09:17:56 +0800

    提交

--- ES6/1.js
+++ ES6/1.js
@@ -294,6 +294,107 @@
 
 
 
+// // JavaScript 能够准确表示的整数范围在-2^53到2^53之间（不含两个端点），超过这个范围，无法精确表示这个值。
+// // ES6 引入了Number.MAX_SAFE_INTEGER和Number.MIN_SAFE_INTEGER这两个常量，用来表示这个范围的上下限。
+// Number.MAX_SAFE_INTEGER === Math.pow(2, 53) - 1
+// // true
+// Number.MAX_SAFE_INTEGER === 9007199254740991
+// // true
+// Number.MIN_SAFE_INTEGER === -Number.MAX_SAFE_INTEGER
+// // true
+// Number.MIN_SAFE_INTEGER === -9007199254740991
+// // true
+// // 上面代码中，可以看到 JavaScript 能够精确表示的极限
+
+// // Number.isSafeInteger()则是用来判断一个整数是否落在这个范围之内。
+// Number.isSafeInteger('a') // false
+// Number.isSafeInteger(null) // false
+// Number.isSafeInteger(NaN) // false
+// Number.isSafeInteger(Infinity) // false
+// Number.isSafeInteger(-Infinity) // false
+
+// Number.isSafeInteger(3) // true
+// Number.isSafeInteger(1.2) // false
+// Number.isSafeInteger(9007199254740990) // true
+// Number.isSafeInteger(9007199254740992) // false
+
+// Number.isSafeInteger(Number.MIN_SAFE_INTEGER - 1) // false
+// Number.isSafeInteger(Number.MIN_SAFE_INTEGER) // true
+// Number.isSafeInteger(Number.MAX_SAFE_INTEGER) // true
+// Number.isSafeInteger(Number.MAX_SAFE_INTEGER + 1) // false
+
+
+
+
+
+
+// Math 对象的扩展
+
+// // 1.Math.trunc方法用于去除一个数的小数部分，返回整数部分。
+// Math.trunc(4.1) // 4
+// Math.trunc(4.9) // 4
+// Math.trunc(-4.1) // -4
+// Math.trunc(-4.9) // -4
+// Math.trunc(-0.1234) // -0
+// // 对于非数值，Math.trunc内部使用Number方法将其先转为数值。
+// Math.trunc('123.456') // 123
+// Math.trunc(true) //1
+// Math.trunc(false) // 0
+// Math.trunc(null) // 0
+// // 对于空值和无法截取整数的值，返回NaN。
+// Math.trunc(NaN);      // NaN
+// Math.trunc('foo');    // NaN
+// Math.trunc();         // NaN
+// Math.trunc(undefined) // NaN
+// // 对于没有部署这个方法的环境，可以用下面的代码模拟
+// Math.trunc = Math.trunc || function(x) {
+//     return x < 0 ? Math.ceil(x) : Math.floor(x);
+//   };
+
+// // 2.Math.sign方法用来判断一个数到底是正数、负数、还是零。对于非数值，会先将其转换为数值。
+// // 它会返回五种值。参数为正数，返回+1；参数为负数，返回-1；参数为 0，返回0； 参数为-0，返回-0;其他值，返回NaN。
+// Math.sign(-5) // -1
+// Math.sign(5) // +1
+// Math.sign(0) // +0
+// Math.sign(-0) // -0
+// Math.sign(NaN) // NaN
+// // 如果参数是非数值，会自动转为数值。对于那些无法转为数值的值，会返回NaN。
+// Math.sign('')  // 0
+// Math.sign(true)  // +1
+// Math.sign(false)  // 0
+// Math.sign(null)  // 0
+// Math.sign('9')  // +1
+// Math.sign('foo')  // NaN
+// Math.sign()  // NaN
+// Math.sign(undefined)  // NaN
+// // 对于没有部署这个方法的环境，可以用下面的代码模拟。
+// Math.sign = Math.sign || function(x) {
+//     x = +x; // convert to a number
+//     if (x === 0 || isNaN(x)) {
+//       return x;
+//     }
+//     return x > 0 ? 1 : -1;
+//   };
+
+// // 3.Math.cbrt方法用于计算一个数的立方根。
+// Math.cbrt(-1) // -1
+// Math.cbrt(0)  // 0
+// Math.cbrt(1)  // 1
+// Math.cbrt(2)  // 1.2599210498948734
+// // 对于非数值，Math.cbrt方法内部也是先使用Number方法将其转为数值。
+// Math.cbrt('8') // 2
+// Math.cbrt('hello') // NaN
+// // 对于没有部署这个方法的环境，可以用下面的代码模拟。
+// Math.cbrt = Math.cbrt || function(x) {
+//     var y = Math.pow(Math.abs(x), 1/3);
+//     return x < 0 ? -y : y;
+//   };
+
+// 4.
+
+
+
+
 
 
 
